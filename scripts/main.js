72091ec8cf0cd5250cdbcefe1b3a2c3a0f158002
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-10-15 06:52:58 +0300

    Added a lot of code. Added translation of values POUNDS and Farenheit.

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ let story = document.querySelector('.story');
 function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
-let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. :Bob: saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 let insertX = ['Willy the Goblin',
 			   'Big Daddy',
 			   'Father Christmas'];
@@ -17,18 +17,27 @@ let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk',
 randomize.addEventListener('click', result);
 
 function result() {
-
-  if(customName.value !== '') {
-    var name = customName.value;
-
-  }
-
-  if(document.getElementById("uk").checked) {
-    var weight = Math.round(300);
-    var temperature =  Math.round(94);
-
-  }
   let newStory = storyText;
-  story.textContent = ;
+  if(customName.value !== '') {
+    let name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
+  if(document.getElementById("uk").checked) {
+    let weight = Math.round(300/2.205);
+    let temperature =  Math.round((94-32)*5/9);
+    newStory = newStory.replace('300 pounds', `${weight} kg`);
+    newStory = newStory.replace('94 fahrenheit', `${temperature} \u00B0`);
+  }
+
+  let 	xitem = randomValueFromArray (insertX),
+  		yitem = randomValueFromArray (insertY),
+  		zitem = randomValueFromArray (insertZ);
+	newStory = newStory.replace('insertx', xitem);
+	newStory = newStory.replace('insertx', xitem);
+	newStory = newStory.replace('inserty', yitem);
+	newStory = newStory.replace('insertz', zitem);
+	console.log(newStory);
+
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
