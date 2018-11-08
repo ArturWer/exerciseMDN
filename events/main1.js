1ca3c5b0b9c31105acacd86ed4c2c49da5191e7f
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-08 08:43:35 +0200

    using properties not addEventListener

--- events/main1.js
+++ events/main1.js
@@ -1,8 +1,14 @@
 let btn = document.querySelector("button");
-    function random(num) {
-      return Math.floor(Math.random()*num);
-    }
-    btn.addEventListener("click", function(){
-      let color = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-      document.body.style.backgroundColor = color;
-    }, false);
+function random(num) {
+  return Math.floor(Math.random()*num);
+}
+/* btn.addEventListener("click", function(){
+  let color = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = color;
+}, false);*/
+function changeBgColor (){
+  let color = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = color;
+};
+btn.onmouseover = changeBgColor;
+
