8aaf7769e711af61183f3fd94b91fd4f2ec34c13
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-09 05:36:27 +0200

    Added a loop for creating 5 small images

--- events/gallery-start/main.js
+++ events/gallery-start/main.js
@@ -5,10 +5,12 @@ btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+for (var i = 1; i <= 5; i++) {
+	let name = `images/pic${i}.jpg`;
 
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('src', name);
   thumbBar.appendChild(newImage);
-
+}
 /* Wiring up the Darken/Lighten button */
 
