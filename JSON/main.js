08be8caa3b65cffa0ccc9d4112cf0c6eba281dc1
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-13 17:05:32 +0200

    Created and send XHR request and get JSON data

--- JSON/main.js
+++ JSON/main.js
@@ -1,2 +1,7 @@
 let header = document.querySelector('header');
 let section = document.querySelector('section');
+var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
