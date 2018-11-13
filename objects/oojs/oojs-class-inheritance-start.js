476fa072bc85c633544db3e6bd9f3e2767b39400
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-13 06:10:26 +0200

    use Classe for Student and Teacer

--- objects/oojs/oojs-class-inheritance-start.js
+++ objects/oojs/oojs-class-inheritance-start.js
@@ -67,18 +67,14 @@ Person.prototype.bio = function() {
 };
 
 var person1 = new Person('Tammi', 'Smith', 17, 'female', ['music', 'skiing', 'kickboxing']);
+
 class Teacher extends Person {
   constructor(first, last, age, gender, interests, subject, grade) {
-    this.name = {
-      first,
-      last
-    };
-  this.age = age;
-  this.gender = gender;
-  this.interests = interests;
-  // subject and grade are specific to Teacher
-  this.subject = subject;
-  this.grade = grade;
+    super(first, last, age, gender, interests);
+
+    // subject and grade are specific to Teacher
+    this.subject = subject;
+    this.grade = grade;
   }
 }
 
@@ -98,8 +94,10 @@ Teacher.prototype.greeting = function() {
 };
 var teacher1 = new Teacher('Dave', 'Griffiths', 31, 'male', ['football', 'cookery'], 'mathematics');
 
-function Student (first, last, age, gender, interests) {
-  Person.call(this, first, last, age, gender, interests);
+class Student extends Person {
+  constructor(first, last, age, gender, interests) {
+    super(first, last, age, gender, interests);
+  }
 }
 Student.prototype = Object.create(Person.prototype);
 Student.prototype.greeting = function() {
@@ -114,3 +112,9 @@ han.greeting();
 let leia = new Person('Leia', 'Organa', 19, 'female', ['Government']);
 leia.farewell();
 // Leia has left the building. Bye for now
+
+let snape = new Teacher('Severus', 'Snape', 58, 'male', ['Potions'], 'Dark arts', 5);
+snape.greeting(); // Hi! I'm Severus.
+snape.farewell(); // Severus has left the building. Bye for now.
+snape.age // 58
+snape.subject; // Dark arts
