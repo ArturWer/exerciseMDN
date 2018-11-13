5c556e845ca77d721afe4a85ea85863244d9b35f
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-13 17:21:48 +0200

    Added request.onload =  function()..

--- JSON/main.js
+++ JSON/main.js
@@ -5,3 +5,8 @@ var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
+request.onload = function() {
+  var superHeroes = request.response;
+  populateHeader(superHeroes);
+  showHeroes(superHeroes);
+}
