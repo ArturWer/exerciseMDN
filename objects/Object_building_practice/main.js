ea2067094848163c42e679f0064381ca416620c2
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-14 15:14:41 +0200

    Added constructor's function Ball

--- objects/Object_building_practice/main.js
+++ objects/Object_building_practice/main.js
@@ -9,6 +9,15 @@ var height = canvas.height = window.innerHeight;
 // function to generate random number
 
 function random(min,max) {
-  var num = Math.floor(Math.random()*(max-min)) + min;
+  var num = Math.floor(Math.random()*(max-min+1)) + min;
   return num;
+}
+
+function Ball(x, y, speedX, speedY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.speedX = speedX;
+  this.speedY = speedY;
+  this.color = color;
+  this.size = size;
 }
