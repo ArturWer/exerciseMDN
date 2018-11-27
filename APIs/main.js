e78cfe7d68e8dff9e2d769937a1c186bb4efe472
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-27 21:30:58 +0200

    created text node and el

--- APIs/main.js
+++ APIs/main.js
@@ -1,3 +1,9 @@
 let link = document.querySelector("a");
 let img = document.querySelector("img");
 link.textContent = 'Mozilla Developer Network';
+
+let sect = document.querySelector("section");
+let para = document.createElement("p");
+let textP = document.createTextNode("I'm a new text Node");
+para.appendChild(textP);
+sect.appendChild(para);
