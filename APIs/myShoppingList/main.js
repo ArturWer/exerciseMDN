ef6bf33be6541b9736a3e2c4c757341bd199f718
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-11-29 11:12:56 +0200

    Realised creating delBtns

--- APIs/myShoppingList/main.js
+++ APIs/myShoppingList/main.js
@@ -1,13 +1,17 @@
 let ul = document.querySelector("ul");
-let btn = document.querySelector("button");
+let btn = document.querySelector(".add");
 let newItem = document.getElementById('newItem');
 
 function createNewEl(text){
 	let li = document.createElement("li");
 	let liText = document.createTextNode(text);
+	let delBtn = document.createElement("button");
+	delBtn.textContent = "DELETE"
 	li.appendChild(liText);
+	li.appendChild(delBtn);
 	ul.appendChild(li);
 }
 btn.addEventListener("click", function(){
 	createNewEl(newItem.value);
+	newItem.value = "";
 });
