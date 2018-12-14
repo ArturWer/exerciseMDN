695ae4d54d841857ba10fc85b68071da61c6ffec
Arthur Vershyhora <ArturiTunes@gmail.com>
2018-12-14 13:01:01 +0200

    Added first running code

--- APIs/ajax-start/main.js
+++ APIs/ajax-start/main.js
@@ -9,6 +9,7 @@ function updateDisplay(verse) {
 	let request = new XMLhttpRequest();
 	request.open("GET", url);
 	request.responseType = 'text';
+	request.send();
 	request.onload = function() {
 	  poemDisplay.textContent = request.response;
 	};
@@ -17,3 +18,5 @@ verseChoose.onchange = function() {
   let verse = verseChoose.value;
   updateDisplay(verse);
 };
+updateDisplay('Verse 1');
+verseChoose.value = 'Verse 1';
